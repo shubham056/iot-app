33933821ff37593604875d9c93c17bf2bbf9fb19
Mintu Bisht <m.bisht0805@gmail.com>
2022-10-14 14:20:30 +0530

    added

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -269,8 +269,8 @@ const Dashboard = () => {
   const callOnce = useRef(true)
   //add root user node 
   useEffect(() => {
-    if (callOnce.current) {
-        callOnce.current = false
+    // if (callOnce.current) {
+    //     callOnce.current = false
    console.log("************************call add root use **************************")
       //callOnce.current = false
       UserService.AddRootUser(userID, { user_name: user.data.profile.first_name + ' ' + user.data.profile.last_name }).then(
@@ -296,7 +296,7 @@ const Dashboard = () => {
             error.toString();
         }
       );
-    }
+   // }
 
   }, [user,userID]);
   //fetch category data
