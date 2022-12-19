ce6648e3114fb58119ddb75f77ed89b5dec3cc9a
Mintu Bisht <m.bisht0805@gmail.com>
2022-12-19 13:17:48 +0530

    added

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1524,7 +1524,8 @@ const Dashboard = () => {
   ));
 
   let addedDevices = Object.values(contentDevice).map((v, i) => (
-    <option value={v.id} selected={renameDeleteId === v.id}>{v.label}</option>
+    // <option value={v.id} selected={renameDeleteId === v.id}>{v.label}</option>
+    <option value={v.id} >{v.label}</option>
   ));
 
   let addedAreas = Object.values(contentArea).map((v, i) => (
