3d5c4af7aa9c75f2ec12384fe4cf91b0b9b75282
Mintu Bisht <m.bisht0805@gmail.com>
2023-02-23 17:15:08 +0530

    added

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -316,7 +316,7 @@ const Dashboard = () => {
 
                 }).catch(err => console.log(err))
 
-              const firstDate = moment().subtract(7,'d').format("YYYY-MM-DD HH:mm:ss");
+              const firstDate = moment().subtract(1, 'd').format("YYYY-MM-DD HH:mm:ss");
               const lastDate = moment().format("YYYY-MM-DD HH:mm:ss");
               //UserService.GetLinkedDeviceData(device_id, "T_power_A")
               UserService.GetLinkedDeviceData(device_id, "T_power_A", format, firstDate, lastDate)
